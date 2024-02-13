2b1dcd11cf5d68e68d79cebf9fbe88d1120c0381
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-13 15:16:55 +0000

    Add tas 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define basic_auth class that mange basic authentication"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic-Authentication class"""
