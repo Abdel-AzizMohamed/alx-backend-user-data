42473608b68924dec2bbc51f13b5008651d592d8
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-21 06:49:50 +0200

    Add tas 0,

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+"""Define a users model"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, VARCHAR
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Define users table"""
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250), nullable=True)
+    reset_token = Column(VARCHAR(250), nullable=True)
