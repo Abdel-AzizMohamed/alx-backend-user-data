9e23bc263d56caa5f14c7b4016458398c3a8ecc1
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-22 19:29:29 +0200

    Add tas 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -103,7 +103,7 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """PUT / reset_password
 
@@ -116,10 +116,9 @@ def update_password() -> str:
 
     try:
         AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
     except ValueError:
         abort(403)
-
-    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
