da71087622684e863992e5cc5c19808ecd0d3dd9
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-22 17:25:21 +0200

    Add tas 11,

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,3 +50,13 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from session id"""
+        if session_id is None:
+            return None
+
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
