89a31bad1c6b6b9c0ed7ada784e3dba79d69ac6e
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-02-22 18:28:59 +0200

    Add tas 17,

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,7 +79,7 @@ class Auth:
             raise ValueError()
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """"""
+        """Updates the given user reset token password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_pass = _hash_password(password)
